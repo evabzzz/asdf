92f2d25646f175e6118e4965b11d7e03ca1d709c
evabzzz <kobiashvilieva@yandex.ru>
2024-12-21 18:20:23 +0300

    Add files via upload
    Добавил файл с прототипами функций lab.h

---
+++ lab.h
@@ -0,0 +1,13 @@
+#ifndef LAB_H
+#define LAB_H
+
+
+void replace_text(const char *filename, const char *old_text, const char *new_text);
+
+
+void delete_lines(const char *filename, const char *text_to_delete);
+
+
+void insert_text(const char *filename, const char *insert_text, int position_front);
+
+#endif 
